87e62779b7e7f61f8ad9c5fd7a9d2c4a8c3d83fe
Abraão Moreira <abraaol.moreira@outlook.com>
2025-08-21 00:39:39 -0300

    feat: add function to rename the last column to 'Observações' and handle NaN values in dataframes

--- Challenge_4/ingest_data.py
+++ Challenge_4/ingest_data.py
@@ -9,6 +9,7 @@ def ingest_excel_files(input_dir="input_data"):
             df_name = os.path.splitext(filename)[0]
             try:
                 df = pd.read_excel(file_path)
+                df = df.fillna('')  # Substitui NaN por ''
                 dataframes[df_name] = df
             except Exception as e:
                 print(f"Erro ao ler {filename}: {e}")
@@ -16,9 +17,19 @@ def ingest_excel_files(input_dir="input_data"):
         print("Nenhum arquivo .xlsx encontrado.")
     return dataframes
 
+def process_admissao_abril(dfs, df_key="ADMISSÃO ABRIL"):
+    if df_key in dfs:
+        df = dfs[df_key]
+        last_col = df.columns[-1]
+        df = df.rename(columns={last_col: "Observações"})
+        dfs[df_key] = df
+
 if __name__ == "__main__":
     dfs = ingest_excel_files()
-    for name, df in dfs.items():
-        print(f"DataFrame '{name}':")
-        print(df.head())
-        print("-" * 40)
+    process_admissao_abril(dfs, df_key="ADMISSÃO ABRIL")
+    print(dfs["ADMISSÃO ABRIL"])
+
+#    for name, df in dfs.items():
+ #       print(f"DataFrame '{name}':")
+  #      print(df.head())
+   #     print("-" * 40)
