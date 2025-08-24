3a6fec26994ed40fe65a4755fcc8a951126d0f82
Abraão Moreira <abraaol.moreira@outlook.com>
2025-08-24 04:52:02 -0300

    feat: add type hints to functions in ingest_data.py for better clarity and maintainability

--- Challenge_4/ingest_data.py
+++ Challenge_4/ingest_data.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-def ingest_excel_files(input_dir="input_data"):
+def ingest_excel_files(input_dir="input_data") -> dict[str, pd.DataFrame]:
     dataframes = {}
     for filename in os.listdir(input_dir):
         if filename.lower().endswith(".xlsx"):
@@ -17,14 +17,14 @@ def ingest_excel_files(input_dir="input_data"):
         print("Nenhum arquivo .xlsx encontrado.")
     return dataframes
 
-def process_admissao_abril(dfs, df_key="ADMISSÃO ABRIL"):
+def process_admissao_abril(dfs: dict[str, pd.DataFrame], df_key="ADMISSÃO ABRIL"):
     if df_key in dfs:
         df = dfs[df_key]
         last_col = df.columns[-1]
         df = df.rename(columns={last_col: "Observações"})
         dfs[df_key] = df
 
-def process_base_dias_uteis(dfs, df_key="Base dias uteis"):
+def process_base_dias_uteis(dfs: dict[str, pd.DataFrame], df_key="Base dias uteis"):
     if df_key in dfs:
         df = dfs[df_key]
         new_headers = df.iloc[0]
@@ -39,9 +39,10 @@ if __name__ == "__main__":
     dfs = ingest_excel_files()
     process_admissao_abril(dfs, df_key="ADMISSÃO ABRIL")
     process_base_dias_uteis(dfs, df_key="Base dias uteis")
-    print(dfs["Base dias uteis"])
+    df = dfs["Base sindicato x valor"]
+    print(df.columns)
 
-#    for name, df in dfs.items():
- #       print(f"DataFrame '{name}':")
-  #      print(df.head())
-   #     print("-" * 40)
+    for name, df in dfs.items():
+      print(f"DataFrame '{name}':")
+      #print(df.head())
+      #print("-" * 40)
