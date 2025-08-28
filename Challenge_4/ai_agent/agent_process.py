e31e0158a0a7e6294c9e16842883f66250fa9851
Abraão Moreira <abraaol.moreira@outlook.com>
2025-08-27 22:56:54 -0300

    feat: refactor agent_process to include additional tools for data processing

--- Challenge_4/ai_agent/agent_process.py
+++ Challenge_4/ai_agent/agent_process.py
@@ -1,5 +1,22 @@
-from ai_agent.tools.processment import consultar_admissao_abril
-from ai_agent.tools.utils import response_to_df
+from ai_agent.tools.processment import (
+    consultar_admissao_abril,
+    consultar_afastamentos,
+    consultar_aprendiz,
+    consultar_ativos,
+    consultar_base_dias_uteis,
+    consultar_base_sindicato_valor,
+    consultar_desligados,
+    consultar_estagio,
+    consultar_exterior,
+    consultar_ferias,
+    consultar_vr_mensal
+)
+from ai_agent.tools.utils import (
+    response_to_df,
+    df_to_excel,
+    df_to_csv,
+    gerar_planilha_excel
+)
 from ai_agent.agent_config import get_agent
 import pandas as pd
 
@@ -7,13 +24,26 @@ def agent_process() -> pd.DataFrame:
 
     tools = [
         consultar_admissao_abril,
-        response_to_df
+        consultar_afastamentos,
+        consultar_aprendiz,
+        consultar_ativos,
+        consultar_base_dias_uteis,
+        consultar_base_sindicato_valor,
+        consultar_desligados,
+        consultar_estagio,
+        consultar_exterior,
+        consultar_ferias,
+        consultar_vr_mensal,
+        response_to_df,
+        df_to_excel,
+        df_to_csv,
+        gerar_planilha_excel
     ]
 
-    query_matriculas = "1. utilizando a ferramenta 'consultar_admissao_abril' liste as matrículas disponíveis."
+    query = "1. utilizando a ferramenta 'consultar_admissao_abril' liste as matrículas disponíveis."
 
     agent = get_agent(tools)
 
-    response = agent.invoke({"input": query_matriculas})
+    response = agent.invoke({"input": query})
 
     return response['output']
