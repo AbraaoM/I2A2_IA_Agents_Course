19979705958d0ee56dac03e5ee10b35f6892cf18
Abraão Moreira <abraaol.moreira@outlook.com>
2025-09-08 14:08:46 -0300

    feat: move DataFrameKeys type definition to a separate module for better organization

--- Challenge_4/singletons/dataframes.py
+++ Challenge_4/singletons/dataframes.py
@@ -1,20 +1,5 @@
 import pandas as pd
 
-from typing import Literal
-
-
-DataFrameKeys = Literal[
-  "admissao_abril",
-    "afastamentos", 
-    "aprendiz",
-    "ativos",
-    "base_dias_uteis",
-    "base_sindicato_valor",
-    "desligados",
-    "estagio",
-    "exterior",
-    "ferias",
-    "vr_mensal"
-]
+from types.DataFrameKeys import DataFrameKeys 
 
 dataframes: dict[DataFrameKeys, pd.DataFrame] = {}
---
+++ Challenge_4/types/DataFrameKeys.py
@@ -0,0 +1,16 @@
+from typing import Literal
+
+
+DataFrameKeys = Literal[
+  "admissao_abril",
+    "afastamentos", 
+    "aprendiz",
+    "ativos",
+    "base_dias_uteis",
+    "base_sindicato_valor",
+    "desligados",
+    "estagio",
+    "exterior",
+    "ferias",
+    "vr_mensal"
+]
