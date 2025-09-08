6ad5ab249fc0fbde195a9eb8353bc085816e14bc
Abraão Moreira <abraaol.moreira@outlook.com>
2025-09-08 14:05:02 -0300

    feat: define DataFrameKeys type for better type safety in dataframes dictionary

--- Challenge_4/singletons/dataframes.py
+++ Challenge_4/singletons/dataframes.py
@@ -1,4 +1,20 @@
 import pandas as pd
 
+from typing import Literal
 
-dataframes: dict[str, pd.DataFrame] = {}
+
+DataFrameKeys = Literal[
+  "admissao_abril",
+    "afastamentos", 
+    "aprendiz",
+    "ativos",
+    "base_dias_uteis",
+    "base_sindicato_valor",
+    "desligados",
+    "estagio",
+    "exterior",
+    "ferias",
+    "vr_mensal"
+]
+
+dataframes: dict[DataFrameKeys, pd.DataFrame] = {}
